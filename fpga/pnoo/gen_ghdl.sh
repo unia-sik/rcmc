10f97aeccdc5c7d94718aebc31e8ba3359143039
Jörg Mische <bobbl@gmx.de>
2019-04-16 01:23:26 +0200

    GHDL Synopsys flag removed

--- fpga/pnoo/gen_ghdl.sh
+++ fpga/pnoo/gen_ghdl.sh
@@ -9,32 +9,30 @@ abs_path() {
 
 
 MYDIR=$(dirname $0)
-GHDL="$(abs_path ${MYDIR}/../../tools/ghdl/bin)/ghdl"
+#GHDL="$(abs_path ${MYDIR}/../../tools/ghdl/bin)/ghdl"
 VHDL_PATH="$(abs_path ${MYDIR}/src)"
-FLAGS="--ieee=synopsys"
+#FLAGS="--ieee=synopsys"
 MYDIR="$(abs_path ${MYDIR})"
-
-
 
 
 cat <<EOL
 #!/bin/sh
 EOL
 
-echo \${GHDL} -a ${FLAGS} constants.vhd
-echo \${GHDL} -a ${FLAGS} clock.vhd
+echo \${GHDL} -a \${GHDL_FLAGS} constants.vhd
+echo \${GHDL} -a \${GHDL_FLAGS} clock.vhd
 
 # common files
 for FILE in LibNode libproc libeu
 do
-    echo \${GHDL} -a ${FLAGS} ${VHDL_PATH}/${FILE}.vhd
+    echo \${GHDL} -a \${GHDL_FLAGS} ${VHDL_PATH}/${FILE}.vhd
 done
 
 # integer pipeline
 for FILE in cpu_top csrfile d_stage e_stage eu f_stage \
   m_stage regfile_ram_replacement regfile w_stage
 do
-    echo \${GHDL} -a ${FLAGS} ${VHDL_PATH}/${FILE}.vhd
+    echo \${GHDL} -a \${GHDL_FLAGS} ${VHDL_PATH}/${FILE}.vhd
 done
 
 # network interface (not required for single core, but don't hurt)
@@ -43,10 +41,10 @@ for FILE in NetworkInterfaceSend NetworkInterfaceRecv flit_buffer \
     pnoo_node_sort pnoo_node_empty pnoo_node_no_rdy pnoo_node_srr \
     pnoo_node_drr pnoo_node_cg
 do
-    echo \${GHDL} -a ${FLAGS} ${VHDL_PATH}/${FILE}.vhd
+    echo \${GHDL} -a \${GHDL_FLAGS} ${VHDL_PATH}/${FILE}.vhd
 done
 
-echo \${GHDL} -a ${FLAGS} dmem_ghdl.vhd
+echo \${GHDL} -a \${GHDL_FLAGS} dmem_ghdl.vhd
 
 echo export GHDL_CYCLE_DRIFT=3
 echo export GHDL_COREID=generate
